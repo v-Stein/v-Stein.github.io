50c7224baefa0e0fa10293aca67e8419e6a5062c
vTripes <victor.steinmann@jiscom.fr>
2018-02-23 17:18:50 +0100

    Add some bundled scripts to OAuth2 things

---
+++ webpack.mix.js
@@ -0,0 +1,15 @@
+const
+    mix = require('laravel-mix'),
+    fs  = require('fs'),
+    p   = require('path'),
+    jp  = 'resources/js',
+    jpp = 'public/js';
+
+for (let b of fs.readdirSync(jp)) {
+    let bp  = p.join(jp, b), // bundlePath is jsPath + bundleName
+        bpp = p.join(jpp, b + '.js'); // bundlePublicPath is jsPublicPath + bundleName + .js
+
+    if (fs.lstatSync(bp).isDirectory()) mix.js(bp, bpp); // If bundle is directory, bundle the scripts contained in it
+}
+
+mix.sass('resources/sass/app.scss', 'public/css');
